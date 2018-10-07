ca0c377ff484192c3c4eb115552d66c34a030f14
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-07 14:06:23 +0530

    Gradle Exercise

---
+++ src/main/java/com/ttn/Employee.java
@@ -0,0 +1,5 @@
+package com.ttn;
+
+public class Employee {
+
+}
---
+++ src/main/java/com/ttn/myTasks.java
@@ -0,0 +1,7 @@
+package com.ttn;
+
+public class myTasks {
+    public static void main(String[] args) {
+        System.out.println("hello world!!");
+    }
+}
